c306d23f021320691715668b04562bb46d28df34
housseindjirdeh <houssein@google.com>
2019-07-12 20:06:37 -0700

    finishes adding vue data

--- src/page-data/vue.js
+++ src/page-data/vue.js
@@ -4,11 +4,17 @@ import ImgBytesBreakdown from "../data/react/all/img-bytes-breakdown";
 import TotalBytesComparison from "../data/react/all/total-bytes-comparison";
 import JSBytesComparison from "../data/react/all/js-bytes-comparison";
 
-import TotalBytesBreakdownNoJquery from "../data/angular/jquery/total-bytes-breakdown";
-import JSBytesBreakdownNoJquery from "../data/angular/jquery/js-bytes-breakdown";
-import ImgBytesBreakdownNoJquery from "../data/angular/jquery/img-bytes-breakdown";
-import TotalBytesComparisonNoJquery from "../data/angular/jquery/total-bytes-comparison";
-import JSBytesComparisonNoJquery from "../data/angular/jquery/js-bytes-comparison";
+import TotalBytesBreakdownNoJquery from "../data/vue/jquery/total-bytes-breakdown";
+import JSBytesBreakdownNoJquery from "../data/vue/jquery/js-bytes-breakdown";
+import ImgBytesBreakdownNoJquery from "../data/vue/jquery/img-bytes-breakdown";
+import TotalBytesComparisonNoJquery from "../data/vue/jquery/total-bytes-comparison";
+import JSBytesComparisonNoJquery from "../data/vue/jquery/js-bytes-comparison";
+
+import TotalBytesBreakdownNuxt from "../data/vue/nuxt/total-bytes-breakdown";
+import JSBytesBreakdownNuxt from "../data/vue/nuxt/js-bytes-breakdown";
+import ImgBytesBreakdownNuxt from "../data/vue/nuxt/img-bytes-breakdown";
+import TotalBytesComparisonNuxt from "../data/vue/nuxt/total-bytes-comparison";
+import JSBytesComparisonNuxt from "../data/vue/nuxt/js-bytes-comparison";
 
 export const headerOptions = [
   'All Vue sites',
@@ -95,77 +101,152 @@ export const data = {
   },
   [headerOptions[1]]: {
     'totalBytesPercentile': {
-      numOrigins: 5239,
-      data: [1737, 2970, 4797],
-      query: 'https://bigquery.cloud.google.com/savedquery/1086077897885:c88deccd0d4c4ca09c64ceaa51245add'
+      numOrigins: 11218,
+      data: [1782, 3405, 5858],
+      query: 'https://bigquery.cloud.google.com/savedquery/1086077897885:28c3eaa8a30742258129f06987a082ca'
     },
     'totalBytesBreakdown': {
-      numOrigins: 5239,
+      numOrigins: 11218,
       data: TotalBytesBreakdownNoJquery,
-      query: 'https://bigquery.cloud.google.com/savedquery/1086077897885:b891eb1edf7e40e0b948ab7d38498056'
+      query: 'https://bigquery.cloud.google.com/savedquery/1086077897885:12cc6e201d3f479f9df887c3913405d1'
     },
     'totalBytesComparison': {
       data: TotalBytesComparisonNoJquery
     },
     'jsBytesPercentile': {
-      numOrigins: 5239,
-      data: [845, 1315, 1874],
-      query: 'https://bigquery.cloud.google.com/savedquery/1086077897885:548b522d10fb40eb94aa60f45c8bad43'
+      numOrigins: 11218,
+      data: [545, 911, 1347],
+      query: 'https://bigquery.cloud.google.com/savedquery/1086077897885:02a95deca24e4a27910b1d306f6065a1'
     },
     'jsBytesBreakdown': {
-      numOrigins: 5239,
+      numOrigins: 11218,
       data: JSBytesBreakdownNoJquery,
-      query: 'https://bigquery.cloud.google.com/savedquery/1086077897885:8edc46e09a774782be0b84c5bf348006'
+      query: 'https://bigquery.cloud.google.com/savedquery/1086077897885:8060313dbf824e1ab2e14112af5d4953'
     },
     'jsBytesComparison': {
-      numOrigins: 5239,
+      numOrigins: 11218,
       data: JSBytesComparisonNoJquery,
-      query: 'https://bigquery.cloud.google.com/savedquery/1086077897885:8edc46e09a774782be0b84c5bf348006'
+      query: 'https://bigquery.cloud.google.com/savedquery/1086077897885:8060313dbf824e1ab2e14112af5d4953'
     },
     'imgBytesPercentile': {
-      numOrigins: 5239,
-      data: [337, 1101, 2660],
-      query: 'https://bigquery.cloud.google.com/savedquery/1086077897885:0ddcd253f7414392b729a290c8088026'
+      numOrigins: 11218,
+      data: [667, 1965, 4299],
+      query: 'https://bigquery.cloud.google.com/savedquery/1086077897885:f5f650ceccb94019b520d6f8f7ebb445'
     },
     'imgBytesBreakdown': {
-      numOrigins: 5239,
+      numOrigins: 11218,
       data: ImgBytesBreakdownNoJquery,
-      query: 'https://bigquery.cloud.google.com/savedquery/1086077897885:49354b1cc7a54975aa1860e546fc829a'
+      query: 'https://bigquery.cloud.google.com/savedquery/1086077897885:eb36e4655a8141b1b9d7f820e616ff9e'
     },
     'optimizedImgOrigins': {
-      numOrigins: 5199,
-      data: [86, 14],
-      query: "https://bigquery.cloud.google.com/savedquery/1086077897885:9376f0a065d645c4b8a5ef87c4bbb776"
+      numOrigins: 11139,
+      data: [76, 24],
+      query: "https://bigquery.cloud.google.com/savedquery/1086077897885:0c62d2af4cfb4602ab526d00a2a45d3a"
     },
     'responsiveImgOrigins': {
-      numOrigins: 5215,
-      data: [79, 21],
-      query: "https://bigquery.cloud.google.com/savedquery/1086077897885:abf752ba64bd4b3cb9a15b406485eb82"
+      numOrigins: 11178,
+      data: [71, 29],
+      query: "https://bigquery.cloud.google.com/savedquery/1086077897885:554e7e7f878149ab8d3b89045d241df1"
     },
     'nextGenImgOrigins': {
-      numOrigins: 5199,
-      data: [60, 40],
-      query: "https://bigquery.cloud.google.com/savedquery/1086077897885:20cd39338bcf42ed8294fc58ed44c97f"
+      numOrigins: 11139,
+      data: [45, 55],
+      query: "https://bigquery.cloud.google.com/savedquery/1086077897885:5c463d8790ef4312ad789a546b315e6b"
     },
     'offscreenImgOrigins': {
-      numOrigins: 5215,
-      data: [84, 16],
-      query: "https://bigquery.cloud.google.com/savedquery/1086077897885:f99019bfd48349788f30d2797c6f7a87"
+      numOrigins: 11178,
+      data: [75, 25],
+      query: "https://bigquery.cloud.google.com/savedquery/1086077897885:50e9e9544c26415697ba6d3b320b07ea"
     },
     'firstContentfulPaint': {
-      numOrigins: 2382,
-      data: [16, 29, 55],
-      query: "https://bigquery.cloud.google.com/savedquery/7475840913:6a1caae5a7904594bf268975de713c8b"
+      numOrigins: 4936,
+      data: [21, 33, 46],
+      query: "https://bigquery.cloud.google.com/savedquery/1086077897885:ed139c0183d4437198ce78bc3d335d0c"
     },
     'firstInputDelay': {
-      numOrigins: 1707,
-      data: [79, 14, 7],
-      query: "https://bigquery.cloud.google.com/savedquery/7475840913:c166d8d8fa0343efaa4ea20b6b3ba55b"
+      numOrigins: 3339,
+      data: [81, 12, 7],
+      query: "https://bigquery.cloud.google.com/savedquery/1086077897885:b72b7fa1c0db4b5f98c5f0a7ec00d602"
     },
     'timeToInteractive': {
-      numOrigins: 5239,
-      data: [17.0, 25.1, 44.8],
-      query: "https://bigquery.cloud.google.com/savedquery/7475840913:e54c12c651de4ba6b3a7a9e6b609befe"
+      numOrigins: 11218,
+      data: [15.4, 23.9, 49.7],
+      query: "https://bigquery.cloud.google.com/savedquery/1086077897885:c738e488a599408f9b0c5776a86eb72b"
+    }
+  },
+  [headerOptions[2]]: {
+    'totalBytesPercentile': {
+      numOrigins: 2710,
+      data: [2050, 3931, 6964],
+      query: 'https://bigquery.cloud.google.com/savedquery/1086077897885:ffc66fd1955c4d32a6a5da6d16df7113'
+    },
+    'totalBytesBreakdown': {
+      numOrigins: 2710,
+      data: TotalBytesBreakdownNuxt,
+      query: 'https://bigquery.cloud.google.com/savedquery/1086077897885:3827763d136e4a47b5899fe476947cc9'
+    },
+    'totalBytesComparison': {
+      data: TotalBytesComparisonNuxt
+    },
+    'jsBytesPercentile': {
+      numOrigins: 2710,
+      data: [639, 1053, 1623],
+      query: 'https://bigquery.cloud.google.com/savedquery/1086077897885:2a5b362af96647aa9ac38a62b523aec0'
+    },
+    'jsBytesBreakdown': {
+      numOrigins: 2710,
+      data: JSBytesBreakdownNuxt,
+      query: 'https://bigquery.cloud.google.com/savedquery/1086077897885:83bacb28fa044bf98f644e28508082bd'
+    },
+    'jsBytesComparison': {
+      numOrigins: 2710,
+      data: JSBytesComparisonNuxt,
+      query: 'https://bigquery.cloud.google.com/savedquery/1086077897885:83bacb28fa044bf98f644e28508082bd'
+    },
+    'imgBytesPercentile': {
+      numOrigins: 2710,
+      data: [740, 1992, 4462],
+      query: 'https://bigquery.cloud.google.com/savedquery/1086077897885:a8cfe05a97004f6ebe4f137b7d395906'
+    },
+    'imgBytesBreakdown': {
+      numOrigins: 2710,
+      data: ImgBytesBreakdownNuxt,
+      query: 'https://bigquery.cloud.google.com/savedquery/1086077897885:31d85aa305534aa0a362d0d9458d2127'
+    },
+    'optimizedImgOrigins': {
+      numOrigins: 2696,
+      data: [73, 27],
+      query: "https://bigquery.cloud.google.com/savedquery/1086077897885:5533b7da439e403092a7f9a5e3f53fc6"
+    },
+    'responsiveImgOrigins': {
+      numOrigins: 2704,
+      data: [68, 32],
+      query: "https://bigquery.cloud.google.com/savedquery/1086077897885:e891cc17c5294aa48606a1d238700b21"
+    },
+    'nextGenImgOrigins': {
+      numOrigins: 2696,
+      data: [42, 58],
+      query: "https://bigquery.cloud.google.com/savedquery/1086077897885:9c1b8691bd384f91bfb04198969a2952"
+    },
+    'offscreenImgOrigins': {
+      numOrigins: 2704,
+      data: [70, 30],
+      query: 'https://bigquery.cloud.google.com/savedquery/1086077897885:3774c9bed78241c493e2917424ebd62d'
+    },
+    'firstContentfulPaint': {
+      numOrigins: 1201,
+      data: [15, 33, 52],
+      query: 'https://bigquery.cloud.google.com/savedquery/1086077897885:7a211590edee4a2c871ca0ce86c772d7'
+    },
+    'firstInputDelay': {
+      numOrigins: 828,
+      data: [75, 15, 10],
+      query: 'https://bigquery.cloud.google.com/savedquery/1086077897885:c835a2caf16c423ca6f411952cf0d967'
+    },
+    'timeToInteractive': {
+      numOrigins: 2710,
+      data: [16.9, 26.2, 57.7],
+      query: 'https://bigquery.cloud.google.com/savedquery/1086077897885:45794c0c1d3e4f7db0f2aaf098cd5467'
     }
   }
 }
